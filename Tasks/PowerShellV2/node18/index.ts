c9d6257bdc6679cdf3b7c2bd8d9e70062744058f
Anatolii Bolshakov (Akvelon INC) <v-anbols@microsoft.com>
2022-05-31 11:47:39 +0300

    Added-test-task-with-several-nodejs-handlers

---
+++ Tasks/PowerShellV2/node18/index.ts
@@ -0,0 +1,33 @@
+import tl = require('azure-pipelines-task-lib/task');
+import { INodeSpecificLogic } from 'multiplehandlerstask';
+import * as MainTaskLogic from "multiplehandlerstask";
+
+class Node6SpecificLogic implements INodeSpecificLogic {
+    copyFiles(source: string, dest: string) {
+        /**
+         * Let's suppose that there are some breaking changes in cp method for node18.
+         * We can rework implementation here to align new behavior with existing
+         */
+
+        tl.cp(source, dest);
+    }
+    getPathInput(name: string, required: boolean): string {
+        
+        return tl.getPathInput(name, required);
+    }
+} 
+
+async function run() {
+    console.log('Using node 18 implementation');
+
+    try {
+        const nodeSpecificLogic = new Node6SpecificLogic();
+        MainTaskLogic.main(nodeSpecificLogic);
+
+        tl.setResult(tl.TaskResult.Succeeded, 'Task completed successfully');
+    } catch (e) {
+        tl.setResult(tl.TaskResult.Failed, `Task failed with an error: ${e}`);
+    }
+}
+
+run();
